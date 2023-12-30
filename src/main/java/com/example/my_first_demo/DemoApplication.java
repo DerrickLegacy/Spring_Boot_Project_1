0b9dc0f16e53c45a36b572d268a9469b458b7185
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-12-30 11:56:10 +0300

    Final Project dependency and maven configurations

--- src/main/java/com/example/my_first_demo/DemoApplication.java
+++ src/main/java/com/example/my_first_demo/DemoApplication.java
@@ -1,13 +1,13 @@
 package com.example.my_first_demo;
 
+// import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
